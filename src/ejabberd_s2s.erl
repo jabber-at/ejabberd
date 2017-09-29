7e94cf94b266fc97e46c52ef6beeaa9af4ed5bd7
Mathias Ertl <mati@er.tl>
2017-09-29 19:27:35 +0200

    New upstream version 17.09

--- src/ejabberd_s2s.erl
+++ src/ejabberd_s2s.erl
@@ -199,9 +199,9 @@ dirty_get_connections() ->
 -spec tls_options(binary(), [proplists:property()]) -> [proplists:property()].
 tls_options(LServer, DefaultOpts) ->
     TLSOpts1 = case ejabberd_config:get_option(
-		      {s2s_certfile, LServer},
+		      {domain_certfile, LServer},
 		      ejabberd_config:get_option(
-			{domain_certfile, LServer})) of
+			{s2s_certfile, LServer})) of
 		   undefined -> DefaultOpts;
 		   CertFile -> lists:keystore(certfile, 1, DefaultOpts,
 					      {certfile, CertFile})
@@ -369,7 +369,7 @@ do_route(Packet) ->
 			    <<"Server connections to local "
 			      "subdomains are forbidden">>, Lang);
 		      forbidden ->
-			  xmpp:err_forbidden(<<"Denied by ACL">>, Lang);
+			  xmpp:err_forbidden(<<"Access denied by service policy">>, Lang);
 		      internal_server_error ->
 			  xmpp:err_internal_server_error()
 		  end,
@@ -726,7 +726,13 @@ opt_type(s2s_use_starttls) ->
 	(false) -> false;
 	(optional) -> optional;
 	(required) -> required;
-	(required_trusted) -> required_trusted
+	(required_trusted) ->
+	    ?WARNING_MSG("The value 'required_trusted' of option "
+			 "'s2s_use_starttls' is deprected and will be "
+			 "unsupported in future releases. Instead, "
+			 "set it to 'required' and make sure "
+			 "mod_s2s_dialback is *NOT* loaded", []),
+	    required_trusted
     end;
 opt_type(s2s_zlib) ->
     fun(B) when is_boolean(B) -> B end;
